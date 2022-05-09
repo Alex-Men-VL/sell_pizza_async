2586b53a64cfed96a62030969c984c6488ab047d
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-05-09 18:28:13 +0300

    Switch the start handler to asynchronous mode

---
+++ tg_bot.py
@@ -0,0 +1,453 @@
+import logging
+import re
+from datetime import datetime
+from textwrap import dedent
+
+import requests
+from environs import Env
+from telegram import (
+    Update,
+    Bot,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    BotCommand
+)
+from telegram.ext import (
+    filters,
+    Application,
+    CallbackContext,
+    CallbackQueryHandler,
+    MessageHandler,
+    CommandHandler,
+    PicklePersistence,
+    PreCheckoutQueryHandler
+)
+
+from logs_handler import TelegramLogsHandler
+from moltin_api import (
+    get_access_token,
+    get_product,
+    get_or_create_cart,
+    add_cart_item,
+    get_cart_items,
+    remove_cart_item,
+    create_customer,
+    delete_cart,
+    get_available_entries,
+    create_flow_entry
+)
+from redis_persistence import RedisPersistence
+from tg_lib import (
+    # send_cart_description,
+    # send_product_description,
+    send_main_menu, parse_cart,
+    # send_delivery_option,
+    # send_order_reminder,
+    # send_payment_invoice,
+    # generate_payment_payload,
+)
+from coordinate_utils import fetch_coordinates, get_nearest_restaurant
+
+logger = logging.getLogger(__file__)
+
+
+async def handle_start(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    chat_id = context.user_data['chat_id']
+    message_id = context.user_data['message_id']
+    moltin_token = context.bot_data['moltin_token']
+    await send_main_menu(context, chat_id, message_id, moltin_token, page=1)
+    context.user_data['current_page'] = 1
+    return 'HANDLE_MENU'
+
+
+# def handle_menu(update, context):
+#     chat_id = context.user_data['chat_id']
+#     message_id = context.user_data['message_id']
+#     user_reply = context.user_data['user_reply']
+#     moltin_token = context.bot_data['moltin_token']
+#
+#     if user_reply == 'cart':
+#         user_cart = get_cart_items(moltin_token, chat_id)
+#         cart_description = parse_cart(user_cart)
+#         send_cart_description(context, cart_description)
+#         return 'HANDLE_CART'
+#     elif user_reply.isdigit():
+#         page = int(user_reply)
+#         context.user_data['current_page'] = page
+#         send_main_menu(context, chat_id, message_id, moltin_token,
+#                        page=page)
+#         return 'HANDLE_MENU'
+#
+#     context.user_data['product_id'] = user_reply
+#
+#     product = get_product(moltin_token, user_reply)
+#     product_main_image = product['data']['relationships'].get('main_image')
+#
+#     product_description = {
+#         'name': product['data']['name'],
+#         'description': product['data']['description'],
+#         'price': product['data']['meta']['display_price']['with_tax'][
+#             'formatted'
+#         ],
+#         'image_id': product_main_image['data']['id'] if product_main_image
+#         else ''
+#     }
+#
+#     send_product_description(context, product_description)
+#     return 'HANDLE_DESCRIPTION'
+#
+#
+# def handle_description(update, context):
+#     chat_id = context.user_data['chat_id']
+#     message_id = context.user_data['message_id']
+#     moltin_token = context.bot_data['moltin_token']
+#     product_id = context.user_data['product_id']
+#     user_reply = context.user_data['user_reply']
+#
+#     if user_reply == 'menu':
+#         current_page = context.user_data['current_page']
+#         send_main_menu(context, chat_id, message_id, moltin_token,
+#                        page=current_page)
+#         return 'HANDLE_MENU'
+#     elif user_reply == 'add':
+#         user_cart = get_or_create_cart(moltin_token, chat_id)
+#         try:
+#             add_cart_item(moltin_token, user_cart['data']['id'],
+#                           product_id, item_quantity=1)
+#         except requests.exceptions.HTTPError:
+#             context.bot.answer_callback_query(
+#                 callback_query_id=update.callback_query.id,
+#                 text='Не удалось добавить товар в корзину'
+#             )
+#         else:
+#             context.bot.answer_callback_query(
+#                 callback_query_id=update.callback_query.id,
+#                 text='Товар добавлен в корзину'
+#             )
+#     return 'HANDLE_DESCRIPTION'
+#
+#
+# def handle_cart(update, context):
+#     chat_id = context.user_data['chat_id']
+#     message_id = context.user_data['message_id']
+#     user_reply = context.user_data['user_reply']
+#     moltin_token = context.bot_data['moltin_token']
+#
+#     if user_reply == 'menu':
+#         current_page = context.user_data['current_page']
+#         send_main_menu(context, chat_id, message_id, moltin_token,
+#                        page=current_page)
+#         return 'HANDLE_MENU'
+#     elif user_reply == 'pay':
+#         if (context.bot_data.get('customers') and
+#                 context.bot_data['customers'].get(chat_id)):
+#             message = 'Пришлите нам ваш адрес текстом или геолокацию.'
+#             context.bot.send_message(text=message,
+#                                      chat_id=chat_id)
+#             context.bot.delete_message(chat_id=chat_id,
+#                                        message_id=message_id)
+#             return 'HANDLE_LOCATION'
+#
+#         message = 'Пожалуйста, напишите свою почту для связи с вами'
+#         context.bot.send_message(text=message,
+#                                  chat_id=chat_id)
+#         context.bot.delete_message(chat_id=chat_id,
+#                                    message_id=message_id)
+#         return 'WAITING_EMAIL'
+#
+#     item_removed = remove_cart_item(moltin_token, chat_id, user_reply)
+#     if item_removed:
+#         context.bot.answer_callback_query(
+#             callback_query_id=update.callback_query.id,
+#             text='Товар удален из корзины'
+#         )
+#         user_cart = get_cart_items(moltin_token, chat_id)
+#         cart_description = parse_cart(user_cart)
+#         send_cart_description(context, cart_description)
+#     else:
+#         context.bot.answer_callback_query(
+#             callback_query_id=update.callback_query.id,
+#             text='Товар не может быть удален из корзины'
+#         )
+#     return 'HANDLE_CART'
+#
+#
+# def handle_email(update, context):
+#     chat_id = context.user_data['chat_id']
+#     user_email = context.user_data['user_reply']
+#     moltin_token = context.bot_data['moltin_token']
+#
+#     email_pattern = r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)'
+#     if not re.fullmatch(email_pattern, user_email):
+#         message = 'Почта указана не верно. Отправьте почту еще раз.'
+#         update.message.reply_text(text=message)
+#         return 'WAITING_EMAIL'
+#
+#     if not context.bot_data.get('customers'):
+#         context.bot_data['customers'] = {}
+#
+#     if not context.bot_data['customers'].get(chat_id):
+#         customer = create_customer(moltin_token, user_email)
+#         context.bot_data['customers'][chat_id] = customer['data']['id']
+#     message = f'''
+#     Вы ввели эту почту: {user_email}
+#
+#     Пришлите нам ваш адрес текстом или геолокацию.'''
+#     update.message.reply_text(text=dedent(message))
+#     return 'HANDLE_LOCATION'
+#
+#
+# def handle_location(update, context):
+#     user_location = context.user_data['user_reply']
+#     moltin_token = context.bot_data['moltin_token']
+#
+#     try:
+#         coordinates = user_location.longitude, user_location.latitude
+#     except AttributeError:
+#         yandex_api_key = context.bot_data['yandex_api_key']
+#         coordinates = fetch_coordinates(user_location, yandex_api_key)
+#     if not coordinates:
+#         update.message.reply_text(
+#             text='Не могу распознать этот адрес, повторите попытку.'
+#         )
+#         return 'HANDLE_LOCATION'
+#
+#     available_restaurants = get_available_entries(moltin_token,
+#                                                   flow_slug='Pizzeria')
+#     nearest_restaurant = get_nearest_restaurant(coordinates,
+#                                                 available_restaurants)
+#     context.user_data.update(
+#         {
+#             'nearest_restaurant': nearest_restaurant,
+#             'delivery_coordinates': coordinates
+#         }
+#     )
+#     lon, lat = coordinates
+#     create_flow_entry(moltin_token, 'Customer-Address',
+#                       {'Lon': lon, 'Lat': lat})
+#     send_delivery_option(update, nearest_restaurant)
+#     return 'HANDLE_DELIVERY'
+#
+#
+# def handle_delivery(update, context):
+#     chat_id = context.user_data['chat_id']
+#     user_reply = context.user_data['user_reply']
+#     moltin_token = context.bot_data['moltin_token']
+#
+#     user_cart = get_cart_items(moltin_token, chat_id)
+#     cart_description = parse_cart(user_cart)
+#     context.user_data['cart_price'] = cart_description['total_price']
+#     nearest_restaurant = context.user_data['nearest_restaurant']
+#
+#     if user_reply == 'pickup':
+#         message = '''
+#         Вы выбрали самовывоз.
+#
+#         Ваш заказ:'''
+#         context.bot.send_message(
+#             chat_id=chat_id,
+#             text=dedent(message)
+#         )
+#         send_cart_description(context, cart_description, with_keyboard=False)
+#         context.bot.send_message(
+#             chat_id=chat_id,
+#             text='Адрес пиццерии:'
+#         )
+#         context.bot.send_location(chat_id,
+#                                   latitude=nearest_restaurant['lat'],
+#                                   longitude=nearest_restaurant['lon'])
+#     elif user_reply == 'delivery':
+#         lon, lat = context.user_data['delivery_coordinates']
+#         courier_id = nearest_restaurant['courier_id']
+#         message = f'''
+#             Новый заказ!
+#
+#             Из ресторана по адресу: {nearest_restaurant['address']}
+#
+#             Содержимое заказа:'''
+#         context.bot.send_message(chat_id=courier_id,
+#                                  text=dedent(message))
+#         send_cart_description(context, cart_description, with_keyboard=False,
+#                               chat_id=courier_id)
+#         context.bot.send_message(
+#             chat_id=courier_id,
+#             text='Адрес заказа:'
+#         )
+#         context.bot.send_location(chat_id=courier_id,
+#                                   latitude=lat,
+#                                   longitude=lon)
+#
+#         context.bot.send_message(
+#             chat_id=chat_id,
+#             text='Спасибо за заказ! Ожидайте доставки'
+#         )
+#         context.job_queue.run_once(send_order_reminder, 3600, context=chat_id)
+#     else:
+#         return 'HANDLE_DELIVERY'
+#
+#     button = [
+#         [InlineKeyboardButton(text='Оплатить', callback_data='pay_now')]
+#     ]
+#     context.bot.send_message(
+#         chat_id=chat_id,
+#         text='Для оплаты нажмите кнопку *Оплатить*',
+#         reply_markup=InlineKeyboardMarkup(button),
+#         parse_mode=ParseMode.MARKDOWN_V2
+#     )
+#
+#     return 'HANDLE_PAYMENT'
+#
+#
+# def handle_payment(update, context):
+#     chat_id = context.user_data['chat_id']
+#     message_id = context.user_data['message_id']
+#     user_reply = context.user_data['user_reply']
+#
+#     if user_reply != 'pay_now':
+#         return 'HANDLE_PAYMENT'
+#
+#     provider_token = context.bot_data['provider_token']
+#     cart_price = context.user_data['cart_price']
+#     payload = generate_payment_payload(update)
+#     send_payment_invoice(context, chat_id, provider_token, cart_price,
+#                          payload=payload)
+#     context.user_data['payload'] = payload
+#     context.bot.delete_message(chat_id=chat_id,
+#                                message_id=message_id)
+#
+#
+# def precheckout_callback(update, context):
+#     query = update.pre_checkout_query
+#     payload = context.user_data['payload']
+#     if query.invoice_payload != payload:
+#         query.answer(ok=False, error_message="Что-то пошло не так...")
+#     else:
+#         query.answer(ok=True)
+#
+#
+# def successful_payment_callback(update, context):
+#     chat_id = update.message.chat_id
+#     moltin_token = context.bot_data['moltin_token']
+#
+#     update.message.reply_text('Оплата прошла успешно')
+#     delete_cart(moltin_token, chat_id)
+
+
+async def handle_users_reply(update: Update,
+                             context: CallbackContext.DEFAULT_TYPE):
+    if message := update.message:
+        user_reply = message.text if message.text else message.location
+        chat_id = message.chat_id
+        message_id = message.message_id
+    elif query := update.callback_query:
+        user_reply = query.data
+        chat_id = query.message.chat_id
+        message_id = query.message.message_id
+    else:
+        return
+
+    context.user_data.update(
+        {
+            'user_reply': user_reply,
+            'chat_id': chat_id,
+            'message_id': message_id
+        }
+    )
+
+    if (not (token_expiration := context.bot_data.get('token_expiration')) or
+            token_expiration <= datetime.timestamp(datetime.now())):
+        moltin_access_token = get_access_token(
+            context.bot_data['client_id'],
+            context.bot_data['client_secret']
+        )
+        context.bot_data.update(
+            {
+                'moltin_token': moltin_access_token['access_token'],
+                'token_expiration': moltin_access_token['expires'],
+            }
+        )
+
+    if user_reply == '/start':
+        user_state = 'START'
+    else:
+        user_state = context.user_data['state']
+    states_functions = {
+        'START': handle_start,
+        # 'HANDLE_MENU': handle_menu,
+        # 'HANDLE_DESCRIPTION': handle_description,
+        # 'HANDLE_CART': handle_cart,
+        # 'WAITING_EMAIL': handle_email,
+        # 'HANDLE_LOCATION': handle_location,
+        # 'HANDLE_DELIVERY': handle_delivery,
+        # 'HANDLE_PAYMENT': handle_payment
+    }
+    state_handler = states_functions[user_state]
+
+    try:
+        next_state = await state_handler(update, context)
+        context.user_data['state'] = next_state
+    except Exception as err:
+        logger.error(err)
+
+
+def main():
+    env = Env()
+    env.read_env()
+
+    logging.basicConfig(level=logging.INFO)
+
+    bot_token = env.str('TG_BOT_TOKEN')
+
+    client_id = env.str('CLIENT_ID')
+    client_secret = env.str('CLIENT_SECRET')
+    yandex_api_key = env.str('YANDEX_API_KEY')
+    provider_token = env.str('PAYMENT_PROVIDER_TOKEN')
+
+    redis_uri = env.str('REDIS_URL')
+
+    initial_db_data = {
+        'bot_data': {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'yandex_api_key': yandex_api_key,
+            'provider_token': provider_token
+        }
+    }
+    persistence = RedisPersistence(url=redis_uri, redis_key='tg',
+                                   initial_data=initial_db_data)
+    application = Application.builder().token(bot_token).persistence(
+        persistence
+    ).build()
+
+    logger.info('Бот запущен')  # TODO: Отправлять логи в спец бот.
+
+    application.add_handler(
+        CallbackQueryHandler(handle_users_reply)
+    )
+    application.add_handler(
+        MessageHandler(filters.TEXT | filters.LOCATION, handle_users_reply)
+    )
+    # application.add_handler(
+    #     CommandHandler('start', handle_users_reply)
+    # )
+    # application.add_handler(
+    #     PreCheckoutQueryHandler(precheckout_callback)
+    # )
+    # application.add_handler(
+    #     MessageHandler(filters.SUCCESSFUL_PAYMENT,
+    #                    successful_payment_callback))
+
+    # await application.bot.delete_my_commands()
+    # await application.bot.set_my_commands(
+    #     language_code='ru',
+    #     commands=[BotCommand('start', 'Перейти в меню')]
+    # )
+
+    try:
+        application.run_polling()
+    except Exception as err:
+        logger.error(err)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tg_lib.py
@@ -0,0 +1,242 @@
+from textwrap import dedent
+
+from more_itertools import chunked
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    LabeledPrice
+)
+
+from moltin_api import (
+    get_product_main_image_url,
+    get_products
+)
+
+
+async def send_main_menu(context, chat_id, message_id, moltin_token, page,
+                         quantity_per_page=8):
+    products = get_products(moltin_token)['data']
+    products_per_page = list(chunked(products, quantity_per_page))
+
+    reply_markup = get_products_menu(products_per_page, page)
+    await context.bot.send_message(text='Пожалуйста, выберите товар:',
+                                   chat_id=chat_id,
+                                   reply_markup=reply_markup)
+    await context.bot.delete_message(chat_id=chat_id,
+                                     message_id=message_id)
+
+
+def get_products_menu(products, page):
+    parsed_products = {
+        product['name']: product['id'] for product in products[page - 1]
+    }
+    keyboard = []
+    for button_name, button_id in parsed_products.items():
+        keyboard.append(
+            [InlineKeyboardButton(text=button_name, callback_data=button_id)]
+        )
+    max_page_number = len(products)
+    previous_page_number = page - 1
+    next_page_number = page + 1
+    if page == 1:
+        previous_page_number = max_page_number
+    elif page == max_page_number:
+        next_page_number = 1
+
+    keyboard.append(
+        [
+            InlineKeyboardButton(text='◀',
+                                 callback_data=previous_page_number),
+            InlineKeyboardButton(text='Корзина', callback_data='cart'),
+            InlineKeyboardButton(text='▶',
+                                 callback_data=next_page_number)
+        ]
+    )
+    return InlineKeyboardMarkup(keyboard)
+
+
+# def send_cart_description(context, cart_description, with_keyboard=True,
+#                           chat_id=None):
+#     cart_items = cart_description['cart_description']
+#     if not cart_items:
+#         message = 'К сожалению, ваша корзина пуста :c'
+#         reply_markup = InlineKeyboardMarkup(
+#             [[InlineKeyboardButton(text='Назад', callback_data='menu')]]
+#         )
+#     else:
+#         message = ''
+#         buttons = []
+#         for item in cart_items:
+#             name = escape_markdown(item['name'], version=2)
+#             description = escape_markdown(item['description'], version=2)
+#             value_price = escape_markdown(item['value_price'], version=2)
+#
+#             message += f'''
+#             *{name}*
+#             _{description}_
+#             {item['quantity']} пицц в корзине на сумму {value_price}
+#
+#             '''
+#             buttons.append([
+#                 InlineKeyboardButton(
+#                     text=f'Убрать из корзины {item["name"]}',
+#                     callback_data=item['id']
+#                 )
+#             ])
+#         total_price = escape_markdown(cart_description["total_price"],
+#                                       version=2)
+#         message += f'*К оплате: {total_price}*'
+#         buttons.append(
+#             [InlineKeyboardButton(text='Оплатить', callback_data='pay')]
+#         )
+#         buttons.append(
+#             [InlineKeyboardButton(text='В меню', callback_data='menu')]
+#         )
+#         reply_markup = InlineKeyboardMarkup(buttons)
+#
+#     chat_id = chat_id if chat_id else context.user_data['chat_id']
+#     message_id = context.user_data['message_id']
+#     reply_markup = reply_markup if with_keyboard else None
+#     context.bot.send_message(chat_id=chat_id,
+#                              text=dedent(message),
+#                              reply_markup=reply_markup,
+#                              parse_mode=ParseMode.MARKDOWN_V2)
+#     context.bot.delete_message(chat_id=chat_id,
+#                                message_id=message_id)
+#
+#
+# def send_product_description(context, product_description):
+#     message = f'''\
+#     {product_description['name']}
+#
+#     Стоимость: {product_description['price']} руб.
+#
+#     {product_description['description']}
+#     '''
+#     chat_id = context.user_data['chat_id']
+#     message_id = context.user_data['message_id']
+#
+#     reply_markup = InlineKeyboardMarkup(
+#         [
+#             [InlineKeyboardButton(text='Положить в корзину',
+#                                   callback_data='add')],
+#
+#             [InlineKeyboardButton(text='В меню', callback_data='menu')]
+#         ]
+#     )
+#
+#     if image_id := product_description['image_id']:
+#         context.bot.send_chat_action(chat_id=chat_id,
+#                                      action='typing')
+#
+#         moltin_token = context.bot_data['moltin_token']
+#         img_url = get_product_main_image_url(moltin_token, image_id)
+#
+#         context.bot.send_photo(chat_id=chat_id,
+#                                photo=img_url,
+#                                caption=dedent(message),
+#                                reply_markup=reply_markup)
+#         context.bot.delete_message(chat_id=chat_id,
+#                                    message_id=message_id)
+#     else:
+#         context.bot.edit_message_text(text=dedent(message),
+#                                       chat_id=chat_id,
+#                                       message_id=message_id,
+#                                       reply_markup=reply_markup)
+#
+#
+# def send_delivery_option(update, restaurant):
+#     distance = restaurant["distance_km"]
+#     if distance < 0.5:
+#         delivery = True
+#         message = f'''
+#         Может, заберете пиццу из нашей пиццерии неподалеку?
+#         Она всего в {'{:.0f}'.format(restaurant['distance_m'])} метрах от вас!
+#         Вот ее адрес: {restaurant['address']}.
+#
+#         А можем и бесплатно доставить, нам не сложно c:'''
+#     elif distance < 5:
+#         delivery = True
+#         message = '''
+#         Похоже, придется ехать  к вам на самокате.
+#         Доставка будет стоить 100 руб.
+#         Доставляем или самовывоз?'''
+#     elif distance < 20:
+#         delivery = True
+#         message = '''
+#         Ближайшая пиццерия довольно далеко от вас.
+#         Доставка будет стоить 200 руб.
+#         Доставляем или самовывоз?'''
+#     else:
+#         delivery = False
+#         message = f'''
+#         Простите, но так далеко мы пиццу не доставим.
+#         Ближайшая пиццерия аж в {'{:.1f}'.format(distance)} километрах от вас!
+#         Будете заказывать самовывоз?'''
+#
+#     buttons = [
+#         [InlineKeyboardButton(text='Самовывоз', callback_data='pickup')]
+#     ]
+#
+#     if delivery:
+#         buttons.append(
+#             [InlineKeyboardButton(text='Доставка', callback_data='delivery')]
+#         )
+#
+#     update.message.reply_text(text=dedent(message),
+#                               reply_markup=InlineKeyboardMarkup(buttons))
+#
+#
+# def send_order_reminder(context):
+#     message = '''
+#     *место для рекламы*
+#     *сообщение что делать если пицца не пришла*'''
+#     context.bot.send_message(chat_id=context.job.context,
+#                              text=dedent(message))
+#
+#
+# def generate_payment_payload(update):
+#     query = update.callback_query.message
+#     first_name = query.chat.first_name
+#     last_name = query.chat.last_name
+#     message_id = query.message_id
+#     return f'{first_name}-{last_name}-{message_id}'
+#
+#
+# def send_payment_invoice(context, chat_id, provider_token, price,
+#                          currency='RUB', payload='Custom-Payload',
+#                          description=None):
+#     title = 'Оплата пиццы'
+#     description = description if description else \
+#         'Здесь должно быть описание заказа'
+#     prices = [LabeledPrice('Pizza', int(float(price)) * 100)]
+#
+#     context.bot.send_invoice(
+#         chat_id, title, description, payload, provider_token, currency, prices
+#     )
+def parse_cart(cart):
+    total_price = cart['meta']['display_price']['with_tax']['formatted']
+    cart_description = []
+
+    for cart_item in cart['data']:
+        item_id = cart_item['id']
+        item_name = cart_item['name']
+        item_description = cart_item['description']
+        item_quantity = cart_item['quantity']
+        item_price = cart_item['meta']['display_price']['with_tax']
+        item_unit_price = item_price['unit']['formatted']
+        item_value_price = item_price['value']['formatted']
+
+        cart_item_description = {
+            'id': item_id,
+            'name': item_name,
+            'description': item_description,
+            'quantity': item_quantity,
+            'unit_price': item_unit_price,
+            'value_price': item_value_price
+        }
+        cart_description.append(cart_item_description)
+    return {
+        'total_price': total_price,
+        'cart_description': cart_description
+    }
